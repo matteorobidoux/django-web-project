4945f3440ed147b8f03558be50a54579b7f96bad
Nic Sarghi <nicsarghi@something.com>
2022-04-19 21:44:13 -0400

    Tested message model and set up a basic listview for the admin dashboard

--- catalog_project/administration/views.py
+++ catalog_project/administration/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.views import generic
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
+
+def dashboard_redirect(request):
+    return redirect('page/1')
 
 # Create your views here.
-def dashboard(request):
-    # Load template from templates/administration/dashboard.html
-    template = loader.get_template('dashboard.html')
-    return HttpResponse(template.render({}, request))
+class Dashboard(generic.ListView):
+    model = User;
+    context_object_name = "user_list"
+    template_name = 'dashboard.html'
+
+    def get_queryset(self):
+        page = self.kwargs["page"]
+        start_users = 5*(page-1)
+        end_users = 5*page
+        return User.objects.all()[start_users:end_users]
+
--- catalog_project/messaging/models.py
+++ catalog_project/messaging/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+CUTOFF_LENGTH = 50
+
 # Create your models here.
 class Message(models.Model):
     content = models.TextField(max_length=500)
     from_user = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='user_sender')
     to_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_reciever')
+
+    def __str__(self):
+        text = self.content[:CUTOFF_LENGTH]
+        return text if len(self.content) <= CUTOFF_LENGTH else text+"..."
