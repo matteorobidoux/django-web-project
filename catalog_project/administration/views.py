fb3639507352cb7b982a06322e2c8c98f29d2c0a
Nic Sarghi <nicsarghi@something.com>
2022-04-19 21:51:55 -0400

    Comments + Added field in Message

--- catalog_project/administration/views.py
+++ catalog_project/administration/views.py
@@ -5,15 +5,18 @@ from django.views import generic
 from django.contrib.auth.models import User
 from django.shortcuts import redirect
 
+# Redirects you to page 1 when you open up the admin dashboard
 def dashboard_redirect(request):
     return redirect('page/1')
 
-# Create your views here.
+# Main dashboard interface when you open the dashboard. Uses the page as an argument to filter users.
 class Dashboard(generic.ListView):
+    # Default model is user.
     model = User;
     context_object_name = "user_list"
     template_name = 'dashboard.html'
 
+    # Get the users based on the page given as an argument
     def get_queryset(self):
         page = self.kwargs["page"]
         start_users = 5*(page-1)
--- catalog_project/messaging/models.py
+++ catalog_project/messaging/models.py
@@ -4,11 +4,15 @@ from django.contrib.auth.models import User
 CUTOFF_LENGTH = 50
 
 # Create your models here.
+
+# Represents messages sent between users
 class Message(models.Model):
     content = models.TextField(max_length=500)
     from_user = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='user_sender')
+    timestamp = models.DateTimeField(auto_now_add=True)
     to_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_reciever')
 
     def __str__(self):
+        # Show ellipses if text is cut off
         text = self.content[:CUTOFF_LENGTH]
         return text if len(self.content) <= CUTOFF_LENGTH else text+"..."
