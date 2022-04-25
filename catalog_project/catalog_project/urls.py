656dc1b758e13dfda91a807e4c066dc9b5f036b8
Deeam Khan <mohammad.khan4@dawsoncollege.qc.ca>
2022-04-25 11:25:14 -0400

    Did login/logout/register

--- catalog_project/catalog_project/urls.py
+++ catalog_project/catalog_project/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', include('administration.urls'), name='admin'),
     path('', include('item_catalog.urls'), name='item_catalog'),
     path('messages/', include('messaging.urls'), name='messaging'),
-    path('manage-users/', include('user_management.urls'), name='user_management'),
+    path('', include('user_management.urls'), name='user_management'),
 ]
--- catalog_project/user_management/views.py
+++ catalog_project/user_management/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 
@@ -6,3 +9,42 @@ from django.template import loader
 def manage_users(request):
     template = loader.get_template('manage-users.html')
     return HttpResponse(template.render({}, request))
+
+def home(request):
+    template_name = 'home.html'
+    member = Profile.objects.get
+    return HttpResponse(template.render({}, request))
+
+def register(request):
+    template_name = 'registration/register.html'
+    if (request.method == 'POST'):
+        reg_form = UserCreationForm(request.POST)
+
+        if(reg_form.is_valid()):
+            user = reg_form.cleaned_data.get('username')
+            reg_form.save()
+            messages.success(request, f'Registration successful for {user}.')
+            return redirect('/login/')
+        messages.error(request, 'Unsuccessful registration. Invalid information.')
+    else:
+        reg_form = UserCreationForm()
+    return render(request=request, template_name=template_name, context={'reg_form':reg_form})
+
+def login_page(request):
+    template_name = 'registration/login.html'
+    if (request.method == 'POST'):
+        login_form = AuthenticationForm(request, data=request.POST)
+        if (login_form.is_valid()):
+            username = login_form.cleaned_data.get('username')
+            password = login_form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if (user is not None):
+                login(request, user)
+                return redirect('/')
+    else:
+        login_form = AuthenticationForm()
+    return render(request=request, template_name=template_name, context={'login_form': login_form})
+
+def logout_page(request):
+    logout(request)
+    return redirect('/')
