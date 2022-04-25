a3924db4a5155e8a66f12c0e3b8aec8ff24dc717
Nic Sarghi <nicsarghi@something.com>
2022-04-25 10:43:39 -0400

    Add pagination

--- catalog_project/administration/views.py
+++ catalog_project/administration/views.py
@@ -16,19 +16,41 @@ class Dashboard(generic.ListView):
     model = User;
     context_object_name = 'user_list'
     template_name = 'dashboard.html'
+    max_per_page = 3
+
+    def get_max_pages(self):
+        return int(len(User.objects.all())/self.max_per_page)+1
 
     # Get the users based on the page given as an argument
     def get_queryset(self):
         page = self.kwargs["page"]
-        start_users = 5*(page-1)
-        end_users = 5*page
+        start_users = self.max_per_page*(page-1)
+        end_users = self.max_per_page*page
         return User.objects.all()[start_users:end_users]
+
+    # Add extra context data such as a page
+    def get_context_data(self, **kwargs):
+        context = super(Dashboard, self).get_context_data(**kwargs)
+        page = self.kwargs["page"]
+        context['page'] = page
+        context['next_page'] = page+1
+        context['last_page'] = page-1
+        context['max_pages'] = self.get_max_pages()
+        return context
 
     def dispatch(self, request, *args, **kwargs):
         issuer = request.user
         in_group = issuer.groups.filter(name="Superuser").exists()
         if not in_group:
             raise Http404("Page not found")
+
+        page = self.kwargs["page"]
+        max_pages = self.get_max_pages()
+        if page < 1 or page > max_pages:
+            page = 1 if page < 1 else page
+            page = max_pages if page > max_pages else page
+            return redirect(f'/admin/page/{page}')
+
         return super(Dashboard, self).dispatch(request, *args, **kwargs)
 
 # Dashboard functions
