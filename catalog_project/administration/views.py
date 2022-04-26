9d349953d5e19d3e5c9561659185535ecf7b8fa1
Nic Sarghi <nicsarghi@something.com>
2022-04-26 13:46:52 -0400

    use permission mixin

--- catalog_project/administration/views.py
+++ catalog_project/administration/views.py
@@ -4,15 +4,17 @@ from django.views import generic, View
 from django.contrib.auth.models import User
 from django.shortcuts import redirect
 from user_management.models import Warning, Profile
+from django.contrib.auth.mixins import PermissionRequiredMixin
 
 # Redirects you to page 1 when you open up the admin dashboard
 def dashboard_redirect(request):
     return redirect('page/1')
 
 # Main dashboard interface when you open the dashboard. Uses the page as an argument to filter users.
-class Dashboard(generic.ListView):
+class Dashboard(PermissionRequiredMixin, generic.ListView):
+    permission_required = "view_dashboard"
     # Default model is user.
-    model = User;
+    model = User
     context_object_name = 'user_list'
     template_name = 'dashboard.html'
     paginate_by = 3
@@ -20,24 +22,17 @@ class Dashboard(generic.ListView):
 """ Dashboard functions """
 
 # View that checks for the permission
-class AuthView(View):
-    # Checks if the user has the permission, throws a 404 if they don't
-    def check_auth(self, request):
-        if not request.user.has_perm(self.permission):
-            return Http404("Page not found")
-
+class AuthView(PermissionRequiredMixin, View):
     # Gets the user object based on kwargs
     def get_user(self, kwargs):
         return User.objects.get(id=kwargs["id"])
 
 # The user warning view
 class WarnUser(AuthView):
-    permission = "warn_user"
+    permission_required = "warn_user"
 
     # Post executes the warning onto the user
     def post(self, request, *args, **kwargs):
-        self.check_auth(request)
-
         message = request.POST.get('warning-message')
         warning = Warning(message=message,user=self.get_user(kwargs))
         warning.save()
@@ -45,8 +40,6 @@ class WarnUser(AuthView):
 
     # Get shows the warning form page
     def get(self, request, *args, **kwargs):
-        self.check_auth(request)
-
         template = loader.get_template('warn-user.html')
         context = {
             'target_user': self.get_user(kwargs)
@@ -54,7 +47,7 @@ class WarnUser(AuthView):
         return HttpResponse(template.render(context, request))
 
 class FlagUser(AuthView):
-    permission = "flag_user"
+    permission_required = "flag_user"
     def post(self, request, *args, **kwargs):
         self.check_auth(request)
         user = self.get_user(kwargs)
@@ -65,7 +58,7 @@ class FlagUser(AuthView):
         return redirect('/admin')
 
 class BlockUser(AuthView):
-    permission = "flag_user"
+    permission_required = "flag_user"
     def post(self, request, *args, **kwargs):
         self.check_auth(request)
         user = self.get_user(kwargs)
@@ -76,7 +69,7 @@ class BlockUser(AuthView):
         return redirect('/admin')
 
 class EditUser(AuthView):
-    permission = "change_user"
+    permission_required = "change_user"
 
     def post(self, request, *args, **kwargs):
         self.check_auth(request)
@@ -85,7 +78,7 @@ class EditUser(AuthView):
         return redirect('/admin')
 
 class DeleteUser(AuthView):
-    permission = "delete_user"
+    permission_required = "delete_user"
 
     # Post executes the deletion
     def post(self, request, *args, **kwargs):
