66abdd028b6ccacaa93912b6d12f2765a58c5dc2
Matteo <sdj00008@gmail.com>
2022-05-04 22:50:26 -0400

    Did all messaging just need to fix small problem and redid comment to comment directly in detail page

--- catalog_project/item_catalog/forms.py
+++ catalog_project/item_catalog/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Rating
+from .models import Rating, Comment
 
 class RateForm(forms.ModelForm):
     class Meta:
         model = Rating
         fields = ['rate']
         exclude = ['user', 'item',]
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        exclude = ['commenter', 'item']
