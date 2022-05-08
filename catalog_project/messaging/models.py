d1afc04f9c788c73be72aff9544fad154396690b
Matteo <sdj00008@gmail.com>
2022-05-07 23:22:11 -0400

    did

--- catalog_project/messaging/models.py
+++ catalog_project/messaging/models.py
@@ -1,7 +1,9 @@
+from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.urls import reverse
+
 
 CUTOFF_LENGTH = 50
 
@@ -12,7 +14,7 @@ class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reciever')
     content = models.TextField(max_length=500, default="")
-    timestamp = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(default = timezone.now)
 
     def get_absolute_url(self):
         return reverse('inbox')
